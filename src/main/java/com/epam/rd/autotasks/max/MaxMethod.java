497425ec2066c686a2db84d8084541a1562afeda
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-07-13 23:07:05 +0300

    test1

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,17 @@
 package com.epam.rd.autotasks.max;
 
+import javax.imageio.stream.ImageInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MaxMethod {
-    public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+    public static int max(int[] values) {//throw new UnsupportedOperationException();
+        int numMax = values[0];
+        for (int num : values) {
+            if (numMax < num){
+                numMax = num;
+            }
+        }
+        return numMax;
     }
 }
